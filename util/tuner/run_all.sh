f495071e6313508aae3669122586d7ed1ab02a4a
Gellert <gellert.li1995@gmail.com>
2025-11-12 13:03:38 -0800

    setup for RTX3090

--- util/tuner/run_all.sh
+++ util/tuner/run_all.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # THIS_DIR="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
+export CUDA_INSTALL_PATH="/usr/local/cuda-12.8/"
+export PATH=$CUDA_INSTALL_PATH/bin:$PATH
 SCRIPT_DIR="./gpu-app-collection-partial/src/cuda/GPU_Microbenchmark/"
 echo "Running make in $SCRIPT_DIR"
 make -C "$SCRIPT_DIR" tuner -j || { echo "make failed"; exit 1; }
